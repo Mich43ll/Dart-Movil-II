45edcfc917b2b7212c702e8efdac4a56f37919b2
Mich43ll <66016616+Mich43ll@users.noreply.github.com>
2023-02-10 20:45:23 +0000

    operadores logicos y operadores condicionales

--- 07-holaMundo.dart
+++ 07-holaMundo.dart
@@ -1,4 +1,46 @@
 void main()
 {
-  print('Hello world');
+  dynamic valor;
+  int x=10;
+  //cambio de signo de la expresion 
+  valor = -x;
+  print(valor);
+
+  //operadores condicionales 
+  /*
+  is " compara el valor de los datos" 
+  is! "compara si el valor de los datos es distintos"
+
+   */
+
+  //Igualdad
+  valor = 10 == 20;
+  print(valor); //false
+
+  var valor2 = '5' == 5;
+  print(valor2); //false
+
+  //Mayor
+  valor = 5 < 7;
+  print(valor); //true
+
+  //Menor
+  valor = 4>3;
+  print(valor); //true
+
+  //<=
+  valor = 10 <= 10;
+  print(valor);//true
+
+  //>=
+  valor = 20 >= 100;
+  print(valor);//false
+
+  //is!
+  valor =10 is! String;
+  print(valor); //true
+
+  //is
+  valor = valor is String;
+  print(valor);
 }
---
+++ 09-operadoresLogicos.dart
@@ -0,0 +1,14 @@
+void main() {
+
+  //OR
+  dynamic valor = false || true;
+  print(valor); //true
+
+  //AND
+  valor = false && true;
+  print(valor); //false
+
+  //!
+  valor = !true;
+  print(valor); //false
+}
