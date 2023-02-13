2ba81f50caa2878e9970d3d6c23214b56fc8f694
Mich43ll <66016616+Mich43ll@users.noreply.github.com>
2023-02-13 20:44:42 +0000

    ingreso de datos

---
+++ ingresoDatos.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+void main(List<String> args) {
+print("Ingrese su nombre: ");
+var nombre = stdin.readLineSync();
+print("Ingrese su edad: ");
+var edad = stdin.readLineSync();
+print("Ingrese su carrera");
+var carrera = stdin.readLineSync();
+print("Ingrese campus al que pertenece: ");
+var campus = stdin.readLineSync();
+print("Mi nombre es $nombre mi edad es $edad y estudio la carrera $carrera en el campus $campus");
+}
