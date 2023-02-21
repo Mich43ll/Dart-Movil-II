68a2dd12c7a7cca47a04e7dd12ac39a5c029e451
Mich43ll <66016616+Mich43ll@users.noreply.github.com>
2023-02-21 04:00:32 +0000

    ingreso de datos con stdin y stdout

--- ingresoDatos.dart
+++ ingresoDatos.dart
@@ -9,4 +9,10 @@ var carrera = stdin.readLineSync();
 print("Ingrese campus al que pertenece: ");
 var campus = stdin.readLineSync();
 print("Mi nombre es $nombre mi edad es $edad y estudio la carrera de $carrera en el campus $campus");
+
+stdout.write("Ingrese su nombre: ");
+nombre = stdin.readLineSync();
+stdout.write("Mi nombre es: $nombre");
+
 }
+
