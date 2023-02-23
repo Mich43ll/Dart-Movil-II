727bda7606c26c0d9e1bbcb792819ec9f5b9b03b
Michaell Osorio <mosoriobaralona@outlook.com>
2023-02-22 21:30:18 -0600

    tarea de ordenar numeros de forma asc y desc

---
+++ condiciones.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  List<String> alumnos = ['Brayan', 'Roberto', 'Oscar'];
+  for (int i = 0; i < alumnos.length; i++) {
+    //Inicio del bucle for
+    print(alumnos[i]);
+  }
+  //Fin del bucle
+
+  //FOR in
+  List<String> autos = ['Toyota', 'Honda', 'Hyundai'];
+  for (String lista in autos) {
+    print(lista);
+  }
+
+  //WHILE
+  dynamic continuar = 's';
+  int contador = 0;
+  while (continuar == 's') {
+    contador++;
+    print("El contador es: $contador");
+    print("Desea continuar: (s/n)");
+    continuar = stdin.readLineSync();
+  }
+
+  //EJERCICIO
+  //Realice una lista que ordene los siguientes datos.
+  //Numero 1,2,3,5,10,14,26
+  //Ascendente y Descendente
+  List<int> numeros = [1, 2, 3, 5, 10, 14, 26];
+
+  print("Lista original: $numeros");
+
+  // Ordenar de manera ascendente
+  numeros.sort();
+  print("Lista ordenada ascendente: $numeros");
+
+  // Ordenar de manera descendente
+  numeros.sort((a, b) => b.compareTo(a));
+  print("Lista ordenada descendente: $numeros");
+}
